3652ab1399dbd5ce05925a15b0499f231effdb83
netgio <gary@netgio.com>
2016-02-29 20:26:50 +0000

    using basic mqtt library

--- plain-mqtt.js
+++ plain-mqtt.js
@@ -1,18 +1,35 @@
 var mqtt    = require('mqtt');
-var client  = mqtt.connect("mqtts://Hackathon.azure-devices.net", 
-    { 
+
+var fs = require("fs");
+
+var mqttConf = JSON.parse(fs.readFileSync("mqtt.conf"));
+var connectionString = mqttConf.connectionString;
+var pubTopic = mqttConf.pubTopic;
+var subTopic = mqttConf.subTopic;
+var clientId = mqttConf.clientId;
+var username = mqttConf.username;
+var password = mqttConf.password;
+
+var myArgs = process.argv.slice(2);
+var payload = myArgs[0];
+
+var conf = { 
        "clean": false, 
-       "clientId":"test", 
-       "username": "Hackathon.azure-devices.net/test/DeviceClientType=azure-iot-device%2F1.0.1",
-       "password": "SharedAccessSignature sr=Hackathon.azure-devices.net/devices/test&sig=%2FWjIdWhXTiS9LutkM7qcQL10oCmNzgtYWIonasHG4BQ%3D&se=1456776593", 
+       "clientId": clientId, 
+       "username": username,
+       "password": password, 
        "protocolId":"MQTT", 
        "protocolVersion":4, 
        "rejectUnauthorized": false
-    });
+    };
+    
+console.log(JSON.stringify(conf));
+
+var client  = mqtt.connect("mqtts://Hackathon.azure-devices.net", conf );
  
 client.on('connect', function () {
   //client.subscribe('presence');
-  client.publish('devices/test/messages/events', 'Hello mqtt');
+  client.publish(pubTopic, payload);
   process.exit(1);
 });
  
