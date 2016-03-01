1eab315be1cda0a1a04e84e17f05a24f682b3c14
netgio <gary@netgio.com>
2016-03-01 11:09:04 +0000

    Mode Serial debug

--- plain-mqtt-serial.js
+++ plain-mqtt-serial.js
@@ -13,41 +13,41 @@ sp.on("data", function (data) {
   console.log("rx'd: " + data);
 });
 
-var myArgs = process.argv.slice(2);
-var payload = myArgs[1];
+// var myArgs = process.argv.slice(2);
+// var payload = myArgs[1];
 
-var mqttConf = JSON.parse(fs.readFileSync(myArgs[0]));
-var connectionString = mqttConf.connectionString;
-var uri = mqttConf.uri;
-var pubTopic = mqttConf.pubTopic;
-var subTopic = mqttConf.subTopic;
-var clientId = mqttConf.clientId;
-var username = mqttConf.username;
-var password = mqttConf.password;
+// var mqttConf = JSON.parse(fs.readFileSync(myArgs[0]));
+// var connectionString = mqttConf.connectionString;
+// var uri = mqttConf.uri;
+// var pubTopic = mqttConf.pubTopic;
+// var subTopic = mqttConf.subTopic;
+// var clientId = mqttConf.clientId;
+// var username = mqttConf.username;
+// var password = mqttConf.password;
 
 
-var conf = { 
-       "clean": false, 
-       "clientId": clientId, 
-       "username": username,
-       "password": password, 
-       "protocolId":"MQTT", 
-       "protocolVersion":4, 
-       "rejectUnauthorized": false
-    };
+// var conf = { 
+//        "clean": false, 
+//        "clientId": clientId, 
+//        "username": username,
+//        "password": password, 
+//        "protocolId":"MQTT", 
+//        "protocolVersion":4, 
+//        "rejectUnauthorized": false
+//     };
     
-console.log(JSON.stringify(conf));
+// console.log(JSON.stringify(conf));
 
-var client  = mqtt.connect(uri, conf );
+// var client  = mqtt.connect(uri, conf );
  
-client.on('connect', function () {
-  //client.subscribe('presence');
-  client.publish(pubTopic, payload);
-  process.exit(1);
-});
+// client.on('connect', function () {
+//   //client.subscribe('presence');
+//   client.publish(pubTopic, payload);
+//   process.exit(1);
+// });
  
-client.on('message', function (topic, message) {
-  // message is Buffer 
-  console.log(message.toString());
-  client.end();
-});
+// client.on('message', function (topic, message) {
+//   // message is Buffer 
+//   console.log(message.toString());
+//   client.end();
+// });
