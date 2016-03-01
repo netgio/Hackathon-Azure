2523d3adc4aa73b05db43395b6269a90f8a04c76
netgio <gary@netgio.com>
2016-03-01 11:37:12 +0000

    Added Serial-MQTT link

--- plain-mqtt-serial.js
+++ plain-mqtt-serial.js
@@ -9,42 +9,43 @@ var sp = new SerialPort("/dev/ttyS0", {
         parser: serialport.parsers.readline("\n")
     }, true);
 
-sp.on("data", function (data) {
-  console.log("rx'd: " + data);
-});
 
-// var myArgs = process.argv.slice(2);
+
+ var myArgs = process.argv.slice(2);
 // var payload = myArgs[1];
 
-// var mqttConf = JSON.parse(fs.readFileSync(myArgs[0]));
-// var connectionString = mqttConf.connectionString;
-// var uri = mqttConf.uri;
-// var pubTopic = mqttConf.pubTopic;
-// var subTopic = mqttConf.subTopic;
-// var clientId = mqttConf.clientId;
-// var username = mqttConf.username;
-// var password = mqttConf.password;
+ var mqttConf = JSON.parse(fs.readFileSync(myArgs[0]));
+var connectionString = mqttConf.connectionString;
+var uri = mqttConf.uri;
+var pubTopic = mqttConf.pubTopic;
+var subTopic = mqttConf.subTopic;
+var clientId = mqttConf.clientId;
+var username = mqttConf.username;
+var password = mqttConf.password;
 
 
-// var conf = { 
-//        "clean": false, 
-//        "clientId": clientId, 
-//        "username": username,
-//        "password": password, 
-//        "protocolId":"MQTT", 
-//        "protocolVersion":4, 
-//        "rejectUnauthorized": false
-//     };
+var conf = { 
+       "clean": false, 
+       "clientId": clientId, 
+       "username": username,
+       "password": password, 
+       "protocolId":"MQTT", 
+       "protocolVersion":4, 
+       "rejectUnauthorized": false
+    };
     
-// console.log(JSON.stringify(conf));
+console.log(JSON.stringify(conf));
 
-// var client  = mqtt.connect(uri, conf );
+var client  = mqtt.connect(uri, conf );
  
-// client.on('connect', function () {
-//   //client.subscribe('presence');
-//   client.publish(pubTopic, payload);
-//   process.exit(1);
-// });
+client.on('connect', function () {
+  // once connected, listen for data on the serial port.
+  sp.on("data", function (data) {
+        //TODO add data validation here
+        client.publish(pubTopic, data);
+        console.log("sending: " + data);
+    });
+});
  
 // client.on('message', function (topic, message) {
 //   // message is Buffer 
